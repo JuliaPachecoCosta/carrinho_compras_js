a271fb04ca206c9c740d602a30943fdd2414448e
JuliaPachecoCosta <julia.p.costa7@aluno.senai.br>
2026-04-09 10:03:14 -0300

    Projeto com erro

--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
-// Inicialização de variáveis
+// Inicialização de variáveis 
 let cart = [];
 let modalQt = 0;
 let key = 0;
 
-// Função auxiliar para selecionar um único elemento dom DOM
+// Função auxiliar para selecionar um único elemento do DOM
 const c = (el) => document.querySelector(el);
 
-// Função auxiliar para selecionar vários elementos do DOM
+// Função auxiliar para selecionar vários elementos do DOM 
 const cs = (el) => document.querySelectorAll(el);
 
-// Manipulação dos dados do modelo a partir do JSON
-modelsJson.map((item, index) => {
+// Manipulação dos dados do modelo a partir do JSON (Usando forEach no lugar de map para melhor performance)
+modelsJson.forEach((item, index) => {
     // Clonar o item do modelo
     let modelsItem = c('.models .models-item').cloneNode(true);
 
     // Atualizar os dados do item do modelo clonado
     modelsItem.setAttribute('data-key', index);
     modelsItem.querySelector('.models-item--img img').src = item.img;
-    modelsItem.querySelector('.models-item--price').innerHTML = `R$ ${item.price[2].toFixed(3)}`;
+    modelsItem.querySelector('.models-item--price').innerHTML = R$ ${item.price[2].toFixed(2)};
     modelsItem.querySelector('.models-item--name').innerHTML = item.name;
     modelsItem.querySelector('.models-item--desc').innerHTML = item.description;
 
-    // Adicionar evento de clique para exibir o modal ao clicar no modelo
+    // Adicionar evento de clique ao exibir o modal ao clicar no modelo
     modelsItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
         key = e.target.closest('.models-item').getAttribute('data-key');
         modalQt = 1;
-
+        
         c('.modelsBig img').src = modelsJson[key].img;
         c('.modelsInfo h1').innerHTML = modelsJson[key].name;
         c('.modelsInfo--desc').innerHTML = modelsJson[key].description;
@@ -34,9 +34,9 @@ modelsJson.map((item, index) => {
 
         // Atualizar os tamanhos disponíveis no modal
         cs('.modelsInfo--size').forEach((size, sizeIndex) => {
-            if (sizeIndex == 2) {
+            if (sizeIndex === 2) {
                 size.classList.add('selected');
-                c('.modelsInfo--actualPrice').innerHTML = `R$ ${modelsJson[key].price[sizeIndex].toFixed(2)}`;
+                c('.modelsInfo--actualPrice').innerHTML = R$ ${modelsJson[key].price[sizeIndex].toFixed(2)};
             }
             size.querySelector('span').innerHTML = modelsJson[key].sizes[sizeIndex];
         });
@@ -47,25 +47,25 @@ modelsJson.map((item, index) => {
         setTimeout(() => {
             c('.modelsWindowArea').style.opacity = 1;
         }, 200);
-
     });
+
     c('.models-area').append(modelsItem);
 });
 
 // Função para fechar o modal
-function closeModal () {
+function closeModal() {
     c('.modelsWindowArea').style.opacity = 0;
     setTimeout(() => {
         c('.modelsWindowArea').style.display = 'none';
     }, 500);
 }
 
-// Adicionar eventos de clique para fechar o modal
+// Adicionar eventos de clique para fechar o modal 
 cs('.modelsInfo--cancelButton, .modelsInfo--cancelMobileButton').forEach((item) => {
     item.addEventListener('click', closeModal);
 });
 
-// Adicionar eventos de clique para controlar a quantidade no modal
+// Adicionar eventos de clique para diminuir a quantidade do modal
 c('.modelsInfo--qtmenos').addEventListener('click', () => {
     if (modalQt > 1) {
         modalQt--;
@@ -73,29 +73,30 @@ c('.modelsInfo--qtmenos').addEventListener('click', () => {
     }
 });
 
+// Adicionar eventos de clique para aumentar a quantidade do modal
 c('.modelsInfo--qtmais').addEventListener('click', () => {
     modalQt++;
     c('.modelsInfo--qt').innerHTML = modalQt;
 });
 
-// Adicionar eventos de clique para selecionar o tamanho do modelo no modal
+// Adicionar eventos de clique para selecionar o tamanho do modelo no modal 
 cs('.modelsInfo--size').forEach((size, sizeIndex) => {
     size.addEventListener('click', (e) => {
         c('.modelsInfo--size.selected').classList.remove('selected');
         size.classList.add('selected');
-        c('.modelsInfo--actualPrice').innerHTML = `R$ ${modelsJson[key].price[sizeIndex].toFixed(2)}`;
+        c('.modelsInfo--actualPrice').innerHTML = R$ ${modelsJson[key].price[sizeIndex].toFixed(2)};
     });
 });
 
-// Adicionar evento de clique para adicionar item ao carrinho
+// Adicionar evento de clique para adicionar o item ao carrinho
 c('.modelsInfo--addButton').addEventListener('click', () => {
-
     // Lógica para adicionar o item ao carrinho
     let size = parseInt(c('.modelsInfo--size.selected').getAttribute('data-key'));
     let identifier = modelsJson[key].id + '@' + size;
-    let locaId = cart.findIndex((item) => item.identifier == identifier);
-    if (locaId > -1) {
-        cart[locaId].qt += modalQt;
+    let localId = cart.findIndex((item) => item.identifier === identifier);
+    
+    if (localId > -1) {
+        cart[localId].qt += modalQt;
     } else {
         cart.push({
             identifier,
@@ -104,100 +105,88 @@ c('.modelsInfo--addButton').addEventListener('click', () => {
             qt: modalQt
         });
     }
+
     updateCart();
     closeModal();
 });
 
 // Evento de clique para exibir o carrinho quando há itens no carrinho
-c('.menu-openner').addEventListener('click', ()=>{
-    if(cart.length > 0){
+c('.menu-openner').addEventListener('click', () => {
+    if (cart.length > 0) {
         c('aside').style.left = '0';
-    };
+    }
 });
 
 // Evento de clique para fechar a barra lateral do carrinho
-c('.menu-closer').addEventListener('click', ()=> {
+c('.menu-closer').addEventListener('click', () => {
     c('aside').style.left = '100vw';
-})
+});
 
 // Evento de clique para finalizar a compra e limpar o carrinho
-c('.cart--finalizar').addEventListener('click', ()=>{
+c('.cart--finalizar').addEventListener('click', () => {
     cart = [];
     updateCart();
-})
+});
 
 // Função para atualizar o conteúdo do carrinho
-function updateCart(){
+function updateCart() {
     // Atualizar o número de itens exibido no ícone do carrinho no menu
     c('.menu-openner span').innerHTML = cart.length;
+    
     // Verificar se há itens no carrinho
-    if(cart.length > 0){
-        // Adicionar a classe 'show' para exibir a barra lateral do carrinho
+    if (cart.length > 0) {
+        // Adicionar a classe 'show' para exibir a barra lateral do carrinho 
         c('aside').classList.add('show');
-        // Limpar o conteúdo atual da lista de itens no carrinho
+        // Limpar o conteúdo atual da lista de itens no carrinho 
         c('.cart').innerHTML = '';
 
-        // Inicializaão de variáveis para os totais
+        // Inicialização de variáveis para os totais
         let subtotal = 0;
         let desconto = 0;
         let total = 0;
 
-        // Iterar sobre os itens no carrinho
-        cart.map((itemCart, index)=>{
+        // Iterar sobre os itens no carrinho 
+        cart.forEach((itemCart, index) => {
             // Encontrar o item correspondente nos dados dos modelos
-            let modelItem = modelsJson.find((itemBD)=>itemBD.id == itemCart.id);
+            let modelItem = modelsJson.find((itemBD) => itemBD.id === itemCart.id);
 
-            // Calcular o subtotal com base no preço e na quantidade do item
+            // Calcular o subtotal com base no preço e na quantidade do item 
             subtotal += modelItem.price[itemCart.size] * itemCart.qt;
 
             // Clonar o item do carrinho para exibição
             let cartItem = c('.models .cart--item').cloneNode(true);
-            let modelSizeName;
 
-            // Determinar o nome do tamanho do modelo com base no índice
-            switch(itemCart.size) {
-                case 0:
-                    modelSizeName = 'P';
-                    break
-                case 1:
-                    modelSizeName = 'M';
-                    break
-                case 2:
-                    modelSizeName = 'G';
-                    break
-            }
-
-            // Atualizar os dados do item clonado do carrinho
+            // Atualizar os dados do item clonado do carrinho (O nome do tamanho é puxado direto do array sizes)
             cartItem.querySelector('img').src = modelItem.img;
-            cartItem.querySelector('.cart--item--nome').innerHTML= `${modelItem.name} - ${modelItem.sizes[itemCart.size]}`;
+            cartItem.querySelector('.cart--item--nome').innerHTML = ${modelItem.name} - ${modelItem.sizes[itemCart.size]};
             cartItem.querySelector('.cart--item--qt').innerHTML = itemCart.qt;
 
             // Adicionar eventos de clique para diminuir e aumentar a quantidade do item no carrinho
-            cartItem.querySelector('.cart--item--qtmenos').addEventListener('click', ()=> {
-                if(itemCart.qt > 1){
-                    itemCart.qt --;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if (itemCart.qt > 1) {
+                    itemCart.qt--;
                 } else {
                     cart.splice(index, 1);
                 }
                 updateCart();
             });
-            cartItem.querySelector('.cart--item--qtmais').addEventListener('click', ()=> {
-                itemCart.qt ++;
+
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                itemCart.qt++;
                 updateCart();
             });
-
-            // Adicionar o item clonado à lista de itens no carrinho
+            
+            // Adicionar o item clonado à lista de itens no carrinho 
             c('.cart').append(cartItem);
         });
 
-        // Calcular o desconto e o total com base no subtotal
         desconto = subtotal * 0.1;
-        total = subtotal - desconto
+        total = subtotal - desconto;
 
         // Atualizar os elementos HTML com os valores de subtotal, desconto e total
-        c('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`
-        c('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`
-        c('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`
+        c('.subtotal span:last-child').innerHTML = R$ ${subtotal.toFixed(2)};
+        c('.desconto span:last-child').innerHTML = R$ ${desconto.toFixed(2)};
+        c('.total span:last-child').innerHTML = R$ ${total.toFixed(2)};
 
     } else {
         // Remover a classe 'show' para ocultar a barra lateral do carrinho
@@ -205,4 +194,4 @@ function updateCart(){
         // Esconder a barra lateral movendo-a para fora da tela
         c('aside').style.left = '100vw';
     }
-};
+}
